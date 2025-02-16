5d28269d3284b701ead7d376942433479e3911ce
Henrique Marques <hmdev132@gmail.com>
2025-02-16 17:05:03 -0300

    chore(setup): initialize Next.js project with TypeScript
    - Configure Next.js with TypeScript
    - Add ESLint configuration
    - Setup styled-components and Framer Motion
    - Configure project dependencies

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { unoptimized: true },
+  compiler: {
+    styledComponents: true,
+  },
+};
+
+module.exports = nextConfig;
